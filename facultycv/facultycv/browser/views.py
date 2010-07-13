864b7886288ee039e87506a4d3b7210de9a3a69b
Christopher Warner <cwarner@kernelcode.com>
2010-07-13 17:22:46 -0400

    Updated pdf view because pisa doesn't support basic net operations

--- facultycv/facultycv/browser/views.py
+++ facultycv/facultycv/browser/views.py
@@ -12,14 +12,21 @@ class PdfView(BrowserView):
 
     def __call__(self):
         response = self.request.response
-        filename = "cv.pdf"
+        title = self.context.Title()
+        ftitle = "/tmp/%s_CV.pdf" % (title)
+        filename = ftitle
+
+        attachment = 'attachment; filename=%s' % (ftitle)
         f = file(filename, "wb")
         pdf_template = ViewPageTemplateFile('pdf-view.pt')(self)
-        #response.setHeader('Content-Type', 'application/pdf')
-        #response.setHeader('Content-Disposition', 'filename=isaw.pdf')
-        pdf = pisa.CreatePDF(pdf_template.encode("UTF-8"), f)
+
+        response.setHeader('Content-Type', 'application/pdf')
+        response.setHeader('Content-Disposition', attachment);
+        pdf = pisa.CreatePDF(pdf_template.encode("UTF-8"), response)
+        f.flush()
         f.close()
-        response.redirect(self.context.absolute_url())
-        if not pdf.err:
-            pisa.startViewer(filename)
-            print self.context.absolute_url()
+        return response
+        #response.redirect(self.context.absolute_url())
+        #if not pdf.err:
+            #response.setHeader('Content-Disposition', ftitle)
+            #response.setHeader('X-Sendfile', filename) 
